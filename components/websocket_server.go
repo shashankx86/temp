b4ad303f2e231744ce36838a1d426b40970bd756
shashankx86 <shashankspis20@gmail.com>
2024-06-28 14:38:09 +0000

    websocket sec. enhancement

--- components/websocket_server.go
+++ components/websocket_server.go
@@ -3,71 +3,75 @@
 package components
 
 import (
-    "log"
-    "os"
-    "os/exec"
-    // "strings"
+	"log"
+	"net/http"
+	"os/user"
+	"os/exec"
 
-    "github.com/creack/pty"
-    "github.com/gorilla/websocket"
-    "net/http"
+	"github.com/creack/pty"
+	"github.com/gorilla/websocket"
+	"github.com/go-chi/cors"
 )
 
 var upgrader = websocket.Upgrader{
-    CheckOrigin: func(r *http.Request) bool {
-        return true
-    },
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
 }
 
 func HandleWebSocket(w http.ResponseWriter, r *http.Request) {
-    conn, err := upgrader.Upgrade(w, r, nil)
-    if err != nil {
-        log.Printf("Failed to upgrade websocket: %v", err)
-        return
-    }
-    defer conn.Close()
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Printf("Failed to upgrade websocket: %v", err)
+		return
+	}
+	defer conn.Close()
 
-    var shell *exec.Cmd
-    var ptyFile *os.File
+	shell := exec.Command("bash")
+	ptyFile, err := pty.Start(shell)
+	if err != nil {
+		log.Printf("Failed to start pty: %v", err)
+		return
+	}
+	defer ptyFile.Close()
 
-    // existingSessions, err := exec.Command("tmux", "ls").Output()
-    // if err == nil && strings.Contains(string(existingSessions), "ncwi-shell") {
-    //     shell = exec.Command("tmux", "attach-session", "-t", "ncwi-shell")
-    // } else {
-    //     shell = exec.Command("tmux", "new-session", "-s", "ncwi-shell", "-d", "resize-pane", "-Z")
-    // }
+	go func() {
+		buf := make([]byte, 1024)
+		for {
+			n, err := ptyFile.Read(buf)
+			if err != nil {
+				return
+			}
+			conn.WriteMessage(websocket.TextMessage, buf[:n])
+		}
+	}()
 
-    shell = exec.Command("bash")
-
-    ptyFile, err = pty.Start(shell)
-    if err != nil {
-        log.Printf("Failed to start pty: %v", err)
-        return
-    }
-    defer ptyFile.Close()
-
-    go func() {
-        buf := make([]byte, 1024)
-        for {
-            n, err := ptyFile.Read(buf)
-            if err != nil {
-                return
-            }
-            conn.WriteMessage(websocket.TextMessage, buf[:n])
-        }
-    }()
-
-    for {
-        _, msg, err := conn.ReadMessage()
-        if err != nil {
-            break
-        }
-        ptyFile.Write(msg)
-    }
+	for {
+		_, msg, err := conn.ReadMessage()
+		if err != nil {
+			break
+		}
+		ptyFile.Write(msg)
+	}
 }
 
 func StartWebSocketServer() {
-    http.HandleFunc("/ws", HandleWebSocket)
-    log.Println("WebSocket server is running on ws://localhost:5492")
-    log.Fatal(http.ListenAndServe(":5492", nil))
+	currentUser, err := user.Current()
+	if err != nil {
+		log.Fatalf("Error getting current user: %v", err)
+	}
+	serverUsername := currentUser.Username
+
+	corsOptions := cors.Options{
+		AllowedOrigins:   []string{"https://ncwi." + serverUsername + ".hackclub.app", "http://localhost"},
+	}
+
+	corsMiddleware := cors.New(corsOptions).Handler
+
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		corsMiddleware(http.HandlerFunc(HandleWebSocket)).ServeHTTP(w, r)
+	})
+
+	log.Println("WebSocket server is running on ws://localhost:5492")
+	log.Fatal(http.ListenAndServe(":5492", nil))
 }
