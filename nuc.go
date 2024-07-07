9fbe6babbf5296400d58fbaafa4460316ac23e52
shashankx86 <shashankspis20@gmail.com>
2024-07-07 14:47:56 +0530

    added pingHandler

--- nuc.go
+++ nuc.go
@@ -84,6 +84,14 @@ func setupLogging() {
     log.Printf("Server started at: %s", time.Now().Format(time.RFC3339))
 }
 
+// Handles the ping requests and returns "pong"
+func pingHandler(w http.ResponseWriter, r *http.Request) {
+    w.Header().Set("Content-Type", "application/json")
+    json.NewEncoder(w).Encode(map[string]string{
+        "message": "pong",
+    })
+}
+
 func main() {
     corsOptions := cors.Options{
         AllowedOrigins:   []string{"*"},
